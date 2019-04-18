5776e6a69641a80d8a89d7774a712e8083805aa8
fahri <elfaatta@gmail.com>
2019-04-18 10:04:31 +0700

    remove sample_pdo dir

--- sample_pdo/templates/part_foot.php
+++
@@ -1,4 +0,0 @@
-<script src="<?=$baseUrl?>/../assets/js/jquery-3.4.0.min.js" crossorigin="anonymous"></script>        
-<script src="<?=$baseUrl?>/../assets/js/bootstrap.js"></script>
-</body>
-</html>
